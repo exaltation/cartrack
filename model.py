e90cdd216243760ade41169565a4e20810ee8884
Jorillo Abdullaev <jorillo311@gmail.com>
2017-09-15 14:44:59 +0500

    +

--- model.py
+++ model.py
@@ -9,9 +9,7 @@ from keras.layers import BatchNormalization
 import common
 # from keras.layers import Dropout
 
-def convolutional_layers():
-    img_input = Input(shape=(None, None, 1))
-
+def convolutional_layers(img_input):
     # 1 layer
     x = Conv2D(48, (5, 5), padding='same', name='conv_1')(img_input)
     x = BatchNormalization()(x)
@@ -38,21 +36,30 @@ def get_training_model():
     Training model acts on a batch of 128x64 windows and outputs a
     1 + 8 * len(CHARS) vector
     """
-    x = convolutional_layers()
+    img_input = Input(shape=(None, None, 1))
+    x = convolutional_layers(img_input)
     x = Dense(2048, activation='relu', name='fc_1')(x)
     presence_idicator = Dense(1, activation='sigmoid', name='presence_idicator')(x)
     encoded_chars = Dense(8 * len(common.CHARS), activation='softmax', name='encoded_chars')(x)
 
-    return Model(inputs=x, outputs=[presence_idicator, encoded_chars])
+    return Model(inputs=img_input, outputs=[presence_idicator, encoded_chars])
 
 def get_detect_model():
     """
     The same as training model, except it acts on arbitary sized image and
     slides the 128x64 window across the image in 8x8 strides
     """
-    x = convolutional_layers()
+    img_input = Input(shape=(None, None, 1))
+    x = convolutional_layers(img_input)
     x = Conv2D(2048, (8, 32), padding="same", strides=(8, 8), activation='relu', name='fc_1')(x)
     presence_idicator = Conv2D(1, (1, 1), activation='sigmoid', name='presence_idicator')(x)
     encoded_chars = Conv2D(8 * len(common.CHARS), (1, 1), activation='softmax', name='encoded_chars')(x)
 
-    return Model(inputs=x, outputs=[presence_idicator, encoded_chars])
+    return Model(inputs=img_input, outputs=[presence_idicator, encoded_chars])
+
+if __name__ == '__main__':
+    training_model = get_training_model()
+    training_model.summary()
+
+    detect_model = get_detect_model()
+    detect_model.summary()
