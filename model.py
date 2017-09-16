0ac6d1611b7bc7314eff36d024eb66e3d4856c40
exaltation <jorillo311@gmail.com>
2017-09-16 15:06:11 +0500

    +

--- model.py
+++ model.py
@@ -52,7 +52,7 @@ def get_detect_model(trained_weights):
     """
     img_input = Input(shape=(None, None, 1))
     x = convolutional_layers(img_input)
-    x = Conv2D(2048, (8, 32), padding="same", strides=(8, 8), activation='relu', name='conv_fc_1')(x)
+    x = Conv2D(2048, (8, 32), padding="valid", strides=(1, 1), activation='relu', name='conv_fc_1')(x)
     presence_idicator = Conv2D(1, (1, 1), activation='sigmoid', name='conv_presence_idicator')(x)
     encoded_chars = Conv2D(8 * len(common.CHARS), (1, 1), activation='softmax', name='conv_encoded_chars')(x)
 
--- train.py
+++ train.py
@@ -2,20 +2,30 @@ import model as models
 import common
 from keras.models import load_model
 
-# trained_model_file = 'model.h5'
-#
-# models.get_training_model().save(trained_model_file)
-#
-# trained_model = load_model(trained_model_file)
-#
-# detect_model = models.get_detect_model(trained_model.get_weights())
-# detect_model.summary()
+def trainprint(s):
+    with open('train.txt','w+') as f:
+        print(s, file=f)
 
-trained_model = models.get_training_model()
-detect_model = models.get_detect_model(123)
+def detectprint(s):
+    with open('detect.txt','w+') as f:
+        print(s, file=f)
 
-fc_1 = trained_model.get_layer('fc_1').get_weights()
-conv_fc_1 = detect_model.get_layer('conv_fc_1').get_weights()
 
-print(fc_1[0].shape)
-print(conv_fc_1[0].shape)
+trained_model_file = 'model.h5'
+
+models.get_training_model().save(trained_model_file)
+
+trained_model = load_model(trained_model_file)
+trained_model.summary(print_fn=trainprint)
+
+detect_model = models.get_detect_model(trained_model.get_weights())
+detect_model.summary(print_fn=detectprint)
+
+# trained_model = models.get_training_model()
+# detect_model = models.get_detect_model(123)
+#
+# fc_1 = trained_model.get_layer('fc_1').get_weights()
+# conv_fc_1 = detect_model.get_layer('conv_fc_1').get_weights()
+#
+# print(fc_1[0].shape)
+# print(conv_fc_1[0].shape)
