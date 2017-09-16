dfb7f40de9af7678b853bbb3c1a8e5e1c82afeff
Jorillo Abdullaev <jorillo311@gmail.com>
2017-09-16 18:06:49 +0500

    +

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ steps_per_epoch = 300
 num_epochs = 300
 validation_steps = 20
 
-def code_to_vec(p, code):
+def code_to_vec(code):
     def char_to_vec(c):
         y = np.zeros((len(common.CHARS),))
         y[common.CHARS.index(c)] = 1.0
@@ -20,19 +20,20 @@ def code_to_vec(p, code):
 
     c = np.vstack([char_to_vec(c) for c in code])
 
-    return np.concatenate([(1. if p else 0), c.flatten()])
+    return c.flatten()
 
 def unzip(b):
-    xs, ys = zip(*b)
+    xs, y1s, y2s = zip(*b)
     xs = np.array(xs)
-    ys = np.array(ys)
-    return xs, ys
+    y1s = np.array(y1s)
+    y2s = np.array(y2s)
+    return xs, {'presence_idicator':y1s, 'encoded_chars':y2s}
 
 def read_batches(batch_size):
     g = generate_ims()
     def gen_vecs():
         for im, c, p in itertools.islice(g, batch_size):
-            yield im, code_to_vec(p, c)
+            yield im.reshape(64, 128, 1), 1. if p else 0, code_to_vec(c)
 
     while True:
         yield unzip(gen_vecs())
@@ -54,7 +55,7 @@ def read_batches(batch_size):
 
 training_model = models.get_training_model()
 training_model.compile(
-    loss='categorical_crossentropy',
+    loss={'presence_idicator':'binary_crossentropy', 'encoded_chars':'categorical_crossentropy'},
     optimizer='adam',
     metrics={'presence_idicator':'binary_accuracy', 'encoded_chars':'categorical_accuracy'})
 
