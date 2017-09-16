ae8115e0f319afc1253afe52e7175c760829b9ff
exaltation <jorillo311@gmail.com>
2017-09-16 13:40:02 +0500

    +

--- train.py
+++ train.py
@@ -2,11 +2,20 @@ import model as models
 import common
 from keras.models import load_model
 
-trained_model_file = 'model.h5'
+# trained_model_file = 'model.h5'
+#
+# models.get_training_model().save(trained_model_file)
+#
+# trained_model = load_model(trained_model_file)
+#
+# detect_model = models.get_detect_model(trained_model.get_weights())
+# detect_model.summary()
 
-models.get_training_model.save(trained_model_file)
+trained_model = models.get_training_model()
+detect_model = models.get_detect_model(123)
 
-trained_model = load_model(trained_model_file)
+fc_1 = trained_model.get_layer('fc_1')
+conv_fc_1 = detect_model.get_layer('conv_fc_1')
 
-detect_model = models.get_detect_model(trained_model.get_weights())
-detect_model.summary()
+print(fc_1[0].shape)
+print(conv_fc_1[0].shape)
