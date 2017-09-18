96f4a388f954a7189d49236bfee625aa0749635d
exaltation <jorillo311@gmail.com>
2017-09-18 15:33:27 +0500

    VGG16

--- model.py
+++ model.py
@@ -10,28 +10,57 @@ from keras.layers import BatchNormalization
 import common
 
 def convolutional_layers(img_input):
-    # 1 layer
-    x = Conv2D(48, (3, 3), padding='same', name='conv_1')(img_input)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = MaxPooling2D(pool_size=(2, 2))(x)
+    # # 1 layer
+    # x = Conv2D(48, (3, 3), padding='same', name='conv_1')(img_input)
+    # x = BatchNormalization()(x)
+    # x = Activation('relu')(x)
+    # x = MaxPooling2D(pool_size=(2, 2))(x)
+    #
+    # # 2 layer
+    # x = Conv2D(64, (3, 3), padding='same', name='conv_2')(x)
+    # x = BatchNormalization()(x)
+    # x = Activation('relu')(x)
+    # x = MaxPooling2D(pool_size=(2, 2))(x)
+    #
+    # # 3 layer
+    # x = Conv2D(128, (3, 3), padding='same', name='conv_3')(x)
+    # x = BatchNormalization()(x)
+    # x = Activation('relu')(x)
+    # x = MaxPooling2D(pool_size=(2, 2))(x)
+    #
+    # # 4 layer
+    # x = Conv2D(256, (3, 3), padding='same', name='conv_4')(x)
+    # x = BatchNormalization()(x)
+    # x = Activation('relu')(x)
 
-    # 2 layer
-    x = Conv2D(64, (3, 3), padding='same', name='conv_2')(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = MaxPooling2D(pool_size=(2, 2))(x)
+    # VGG16 model
+    # Block 1
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
 
-    # 3 layer
-    x = Conv2D(128, (3, 3), padding='same', name='conv_3')(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = MaxPooling2D(pool_size=(2, 2))(x)
+    # Block 2
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
 
-    # 4 layer
-    x = Conv2D(256, (3, 3), padding='same', name='conv_4')(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
+    # Block 3
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+
+    # Block 4
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+
+    # Block 5
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
 
     return x
 
@@ -42,8 +71,14 @@ def get_training_model():
     """
     img_input = Input(shape=(64, 128, 1))
     x = convolutional_layers(img_input)
-    x = Flatten()(x)
-    x = Dense(2048, activation='relu', name='fc_1')(x)
+
+    # x = Flatten()(x)
+    # x = Dense(2048, activation='relu', name='fc_1')(x)
+
+    x = Flatten(name='flatten')(x)
+    x = Dense(4096, activation='relu', name='fc1')(x)
+    x = Dense(4096, activation='relu', name='fc2')(x)
+
     presence_indicator = Dense(1, activation='sigmoid', name='presence_indicator')(x)
     encoded_chars = Dense(8 * len(common.CHARS), activation='softmax', name='encoded_chars')(x)
 
@@ -53,7 +88,9 @@ def get_detect_model(trained_weights):
     """
     The same as training model, except it acts on arbitary sized image and
     slides the 128x64 window across the image in 8x8 strides
-    TODO: load trained weights
+
+    not usable!
+    TODO: restructurize model and load trained weights!!!
     """
     img_input = Input(shape=(None, None, 1))
     x = convolutional_layers(img_input)
