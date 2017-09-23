a7fbce445f5eb4037367ab918bd45ba3b715a439
Jorillo Abdullaev <jorillo311@gmail.com>
2017-09-23 14:29:12 +0500

    +

--- detect.py
+++ detect.py
@@ -1,32 +1,29 @@
 from model import get_detect_model
 from common import CHARS
 import numpy as np
+import cv2
+import sys
 
 weights_file = 'model_weights_fc1_7.h5'
 
-def make_scaled_ims(im, min_shape):
-    ratio = 1. / 2 ** 0.5
-    shape = (im.shape[0] / ratio, im.shape[1] / ratio)
-
-    result = []
-
-    while True:
-        shape = (int(shape[0] * ratio), int(shape[1] * ratio))
-        if shape[0] < min_shape[0] or shape[1] < min_shape[1]:
-            break
-        result.append(cv2.resize(im, (shape[1], shape[0])))
-
-    return np.array(result)
-
 def detect(image):
-    scaled_ims = make_scaled_ims(im, (64, 128))
-
+    print(image.shape)
+    im = image.reshape(1, image.shape[0], image.shape[1], 1)
     detect_model = get_detect_model(weights_file)
-    y_vals = detect_model.predict_on_batch(scaled_ims)
-
-    for i, (scaled_im, y_val) in enumerate(scaled_ims, y_vals):
-        print(y_val['conv_presence_indicator'])
+    y_val = detect_model.predict(im, batch_size=1)
+    print(len(y_val))
+    print(y_val[0].shape)
+    print(y_val[0][0][15][21][0])
+    print(y_val[1].shape)
+    print(y_val[2].shape)
+    print(y_val[3].shape)
+    # for i, (scaled_im, y_val) in enumerate(scaled_ims, y_vals):
+    #     print(y_val['conv_presence_indicator'])
+    #     if i > 25:
+    #         return
 
 if __name__ == '__main__':
     im = cv2.imread(sys.argv[1])
     im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) / 255.
+
+    detect(im_gray)
